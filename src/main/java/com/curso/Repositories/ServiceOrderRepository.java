a9fd1a092b181531706d8c5c9790e2f218ec7f67
jeffersonarpasserini <jefferson.passerini@gmail.com>
2024-03-14 14:12:37 -0300

    alteração service order repository

--- src/main/java/com/curso/Repositories/ServiceOrderRepository.java
+++ src/main/java/com/curso/Repositories/ServiceOrderRepository.java
@@ -2,7 +2,6 @@ package com.curso.Repositories;
 
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.curso.domains.ServiceOrder;
 
 public interface ServiceOrderRepository extends JpaRepository<ServiceOrder, UUID> {
